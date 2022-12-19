8b6d6d1113a25f8792d7ecf154467dbb09434351
Thomas Brandon <tsbrandon1010@gmail.com>
2022-12-18 22:27:05 -0500

    added QR code orientation

--- main.py
+++ main.py
@@ -1,27 +1,62 @@
 import cv2 as cv
-import matplotlib.pyplot as plt
+import numpy as np
 
 # https://www.nayuki.io/page/qr-code-generator-library
 
+def processImage(fImagePath: str):
+    img = cv.imread(fImagePath)
+    grayScale = cv.cvtColor(img, cv.COLOR_BGRA2GRAY)
+    
+    return img, grayScale
+
+def findContours(fGrayScale: cv.Mat):
+    contour, _ = cv.findContours(fGrayScale, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    return contour
+
+def drawContours(fImage: cv.Mat, fContour: any):
+    for i in range(len(fContour)):
+        cv.drawContours(fImage, fContour, i, (0,0,255), 1)
+        cv.waitKey(200)
+        cv.imshow("output", fImage)
+
+
+# using masking to determine the orientation of the QR code
+# passing in the QR code to be read
+def orientateImage(fImage: cv.Mat) -> cv.Mat:
+    
+    masking, grayScale = processImage('images/orientationMask.png')
+    
+    _, orientation = processImage('images/orientation.png')
+
+    correctOrientation = False
+    while not correctOrientation:
+        fImage = cv.rotate(fImage, cv.ROTATE_90_CLOCKWISE)
+        mask = cv.bitwise_and(masking, fImage)
+        
+        # I need to look into a more efficent way of converting types
+        # here I am converting from a numpy array to a cv2 Mat
+        cv.imwrite('images/testing.png', mask)
+        _, mask = processImage('images/testing.png')
+        difference = cv.subtract(mask, orientation)
+        correctOrientation = not np.any(difference)
+    
+    return fImage
+
+
 def main ():
 
-    imgOne = cv.imread('images/frame.png')
-    imgTwo = cv.imread('images/frameTwo.png')
-
-    grayScl = cv.cvtColor(imgOne, cv.COLOR_BGR2GRAY)
-    cont, _ = cv.findContours(grayScl, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    
     
     # not sure if I need to be finding all of the contours
     # might be easier to assume the QR code is always in the correct orientation, and check pixels in a grid
 
+    incorrectOrientation = 'images/eastOrientation.png'
     
-    for i in range(len(cont)):
-        
-        cv.drawContours(imgOne, cont, i, (0,0,255), 1)
-        cv.imshow('test', imgOne)
-        print(cont[i])
-        cv.waitKey(200)
+    processedImage, grayScal = processImage(incorrectOrientation)
+    
+    cv.imshow('before-orientation', processedImage)
+
+    correctOrientation = orientateImage(processedImage)
+    cv.imshow('after-orientation', correctOrientation)
 
     cv.waitKey(0)
 
