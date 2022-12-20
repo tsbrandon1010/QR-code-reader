bfbc1cd03c26a7b2eff5b314a0fe54b83484fea9
Thomas Brandon <tsbrandon1010@gmail.com>
2022-12-20 10:55:47 -0500

    making it more pythonic

--- main.py
+++ main.py
@@ -3,61 +3,79 @@ import numpy as np
 
 # https://www.nayuki.io/page/qr-code-generator-library
 
-def processImage(fImagePath: str):
-    img = cv.imread(fImagePath)
-    grayScale = cv.cvtColor(img, cv.COLOR_BGRA2GRAY)
+class QRCode:
+
+    # https://i.stack.imgur.com/N4RFU.png
+    masking_pattern = 0
+
+    def __init__(self, file_path: str) -> None:
+        self.image_path = file_path
+        self.image, self.grayscale = self.process_image(self.image_path)        
+        self.orientate_image()
+
+    @staticmethod
+    def process_image(image_path: str):
+        img = cv.imread(image_path)
+        grayscale = cv.cvtColor(img, cv.COLOR_BGRA2GRAY)
+        return img, grayscale
     
-    return img, grayScale
+    @staticmethod
+    def find_contours(grayscale: cv.Mat):
+        contour, _ = cv.findContours(grayscale, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+        return contour
 
-def findContours(fGrayScale: cv.Mat):
-    contour, _ = cv.findContours(fGrayScale, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    return contour
-
-def drawContours(fImage: cv.Mat, fContour: any):
-    for i in range(len(fContour)):
-        cv.drawContours(fImage, fContour, i, (0,0,255), 1)
-        cv.waitKey(200)
-        cv.imshow("output", fImage)
+    @staticmethod
+    def draw_contours(image: cv.Mat, contour: any):
+        for i in range(len(contour)):
+            cv.drawContours(image, contour, i, (0,0,255), 1)
+            cv.waitKey(200)
+            cv.imshow("output", image)
 
 
-# using masking to determine the orientation of the QR code
-# passing in the QR code to be read
-def orientateImage(fImage: cv.Mat) -> cv.Mat:
-    
-    masking, grayScale = processImage('images/orientationMask.png')
-    
-    _, orientation = processImage('images/orientation.png')
-
-    correctOrientation = False
-    while not correctOrientation:
-        fImage = cv.rotate(fImage, cv.ROTATE_90_CLOCKWISE)
-        mask = cv.bitwise_and(masking, fImage)
+    # using masking to determine the orientation of the QR code
+    # passing in the QR code to be read
+    def orientate_image(self):
         
-        # I need to look into a more efficent way of converting types
-        # here I am converting from a numpy array to a cv2 Mat
-        cv.imwrite('images/testing.png', mask)
-        _, mask = processImage('images/testing.png')
-        difference = cv.subtract(mask, orientation)
-        correctOrientation = not np.any(difference)
-    
-    return fImage
+        masking, mask_grayscale = self.process_image('images/orientationMask.png')
+        
+        _, orientation_reference = self.process_image('images/orientation.png')
+
+        correct_orientation = False
+        rotated_image = self.image
+        while not correct_orientation:
+            rotated_image = cv.rotate(rotated_image, cv.ROTATE_90_CLOCKWISE)
+            mask = cv.bitwise_and(masking, rotated_image)
+            
+            # I need to look into a more efficient way of converting types
+            # here I am converting from a numpy array to a cv2 Mat
+            # 'testing.png' should probably be deleted when we are done using it
+            cv.imwrite('images/testing.png', mask)
+            _, mask = self.process_image('images/testing.png')
+            
+            
+            difference = cv.subtract(mask, orientation_reference)
+            correct_orientation = not np.any(difference)
+        
+        self.image = rotated_image
+
+    # we need to read the pixels from the image into a hash map, where the key is the simplified coordinate, and the value is 0 or 1
+    def map_image(fImage: cv.Mat):
+
+        
+        pass
+
+    # within the array we need to determine the masking pattern, then change the bits within the hash map accordingly
 
 
 def main ():
 
-    
-    # not sure if I need to be finding all of the contours
-    # might be easier to assume the QR code is always in the correct orientation, and check pixels in a grid
+    incorrect_orientation = 'images/eastOrientation.png'
+    img, _ = QRCode.process_image(incorrect_orientation)
+    cv.imshow('before correction', img)
 
-    incorrectOrientation = 'images/eastOrientation.png'
+    qr_code = QRCode(incorrect_orientation)
+    cv.imshow('after correction', qr_code.image)
     
-    processedImage, grayScal = processImage(incorrectOrientation)
-    
-    cv.imshow('before-orientation', processedImage)
-
-    correctOrientation = orientateImage(processedImage)
-    cv.imshow('after-orientation', correctOrientation)
-
     cv.waitKey(0)
 
 if __name__ == '__main__':
